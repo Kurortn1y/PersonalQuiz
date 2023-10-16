1696d69a516e52d902d4313a47d97da57ecd9d1f
RomaRoma <lastmandanc1ng@icloud.com>
2023-10-16 14:19:49 +0200

    Final final

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -17,46 +17,34 @@ final class ResultViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var animals = getAnimals(from: chosenAnswers)
-        var finalAnimals = getAnimalsCount(from: animals)
-        var resultAnimal = getMainAnimalAnswer(from: finalAnimals)
+        navigationItem.hidesBackButton = true
+        let resultAnimal = getMainAnimalAnswer(from: chosenAnswers)
         resultDefinition.text = resultAnimal?.definition
         resultLabel.text = "Вы - \(resultAnimal?.rawValue.description ?? "")"
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.navigationItem.setHidesBackButton(true, animated: true)
-    }
+
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
     
-    
     deinit {
         print("\(type(of: self)) has been deallocated")
     }
     
-    private func getAnimals(from answers: [Answer]) -> [Animal] {
-        return answers.map { $0.animal }
-    }
-    
-    private func getAnimalsCount(from animals: [Animal]) -> [Animal: Int] {
-        var frequencyForAnmal: [Animal : Int] = [:]
-        
-        for animal in animals {
-            if let count = frequencyForAnmal[animal] {
-                frequencyForAnmal[animal] = count + 1
+    private func getMainAnimalAnswer(from answers: [Answer]) -> Animal? {
+        var frequencyForAnimal: [Animal: Int] = [:]
+
+        for answer in answers {
+            let animal = answer.animal
+            if let count = frequencyForAnimal[animal] {
+                frequencyForAnimal[animal] = count + 1
             } else {
-                frequencyForAnmal[animal] = 1
+                frequencyForAnimal[animal] = 1
             }
         }
-        return frequencyForAnmal
-    }
-    
-    private func getMainAnimalAnswer(from animals: [Animal : Int]) -> Animal?{
-        let mainAnimalAnswer = animals.max(by: { $0.value < $1.value })
+
+        let mainAnimalAnswer = frequencyForAnimal.max(by: { $0.value < $1.value })
         return mainAnimalAnswer?.key
     }
     
